7fb01ae276b0a9ccf77f1d36c104789902217d26
Chris Yang <yy4382@outlook.com>
2024-04-19 21:12:57 +0800

    feat: check s3 settings before use

--- utils/newClient.ts
+++ utils/newClient.ts
@@ -1,7 +1,8 @@
-import { type S3Config } from "~/types";
+import { type S3Config, s3ConfigSchema } from "~/types";
 import { S3Client } from "@aws-sdk/client-s3";
 
 export default function (s3Settings: S3Config): S3Client {
+  s3ConfigSchema.parse(s3Settings);
   return new S3Client({
     region: s3Settings.region,
     credentials: {
