60c595ed32be59c77e8f49a7b60dc41e1da29ee6
Chris Yang <yy4382@outlook.com>
2024-04-21 13:26:03 +0800

    feat: header and footer style

--- components/FooterComp.vue
+++ components/FooterComp.vue
@@ -1,12 +1,18 @@
 <template>
-  <div
-    class="p-3 text-center bg-surface-50 dark:bg-surface-700 text-surface-900 dark:text-surface-100 rounded-t-md"
-  >
-    <p>
-      Made by
-      <a href="https://yfi.moe" class="hover:text-primary-400 transition"
-        >Yunfi</a
-      >.
-    </p>
-  </div>
+  <UContainer class="w-full pb-2 sm:pb-3 lg:pb-4">
+    <UCard
+      :ui="{
+        body: {
+          base: '',
+          background: '',
+          padding: 'px-2 py-2 sm:p-2 sm:px-4',
+        },
+      }"
+    >
+      <div class="inline-flex items-center justify-between w-full dark:text-gray-400 text-gray-600 text-sm">
+        <span>© 2024 <a href="https://yfi.moe">Yunfi</a><br>Powered by Nuxt & Nuxt UI. Hosted on Vercel.</span>
+        <span>Source at <a href="https://github.com/yy4382/s3-image-port">GitHub</a></span>
+      </div>
+    </UCard>
+  </UContainer>
 </template>
--- components/NavMenu.vue
+++ components/NavMenu.vue
@@ -1,18 +1,19 @@
 <template>
   <UContainer class="w-full">
     <nav class="flex items-center justify-between h-16">
-      <div class="flex-1 justify-start">
+      <div class="flex-1 justify-start flex items-center gap-2">
         <Icon
           name="i-mingcute-photo-album-fill"
           class="text-3xl text-primary"
         />
+        <span class="text-xl font-bold hidden md:block">S3 Image Port</span>
       </div>
       <div class="flex space-x-4">
         <ULink
           to="/"
           active-class="text-primary"
           inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200"
-          >Home</ULink
+          >Upload</ULink
         >
         <ULink
           to="/photos"
