1ee253ee40229fc2f7b46edb31d276ea4d9779b3
Chris Yang <yy4382@outlook.com>
2024-04-21 12:36:29 +0800

    feat: add seo metas

--- app.vue
+++ app.vue
@@ -6,3 +6,31 @@
   </div>
   <UNotifications />
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: "S3 Image Port",
+  description: "A dashboard to manage your images in S3 buckets. Can be used as a image hosting service.",
+  ogTitle: "S3 Image Port",
+  ogDescription: "A dashboard to manage your images in S3 buckets.",
+  ogImage: "/cover.png",
+  ogUrl: "https://iport.yfi.moe",
+  twitterTitle: "S3 Image Port",
+  twitterDescription: "A dashboard to manage your images in S3 buckets.",
+  twitterImage: "/cover.png",
+  twitterCard: "summary_large_image",
+});
+
+useHead({
+  htmlAttrs: {
+    lang: "en",
+  },
+  link: [
+    {
+      rel: "icon",
+      type: "image/x-icon",
+      href: "/favicon.ico",
+    },
+  ],
+});
+</script>
