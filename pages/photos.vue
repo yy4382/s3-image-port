cbf8fac39ead1881cbed3375b8756abd9b19c333
Chris Yang <yy4382@outlook.com>
2024-04-30 23:13:39 +0800

    feat: add client only to some components to avoid hydration error

--- pages/photos.vue
+++ pages/photos.vue
@@ -1,28 +1,30 @@
 <template>
   <UContainer class="space-y-4">
-    <UButton
-      :label="photos.length === 0 ? 'Load' : 'Refresh'"
-      @click="listPhotos"
-      variant="outline"
-    />
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
-      <div v-for="photo in photos.slice((page - 1) * 9, page * 9)">
-        <PhotoCard :photo="photo" @delete-photo="deletePhoto" />
+    <ClientOnly>
+      <UButton
+        :label="photos.length === 0 ? 'Load' : 'Refresh'"
+        @click="listPhotos"
+        variant="outline"
+      />
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
+        <div v-for="photo in photos.slice((page - 1) * 9, page * 9)">
+          <PhotoCard :photo="photo" @delete-photo="deletePhoto" />
+        </div>
       </div>
-    </div>
-    <UPagination
-      class="mx-auto max-w-fit"
-      v-model="page"
-      :total="photos.length"
-      :perPage="9"
-      v-if="photos.length > 0"
-    />
+      <UPagination
+        class="mx-auto max-w-fit"
+        v-model="page"
+        :total="photos.length"
+        :perPage="9"
+        v-if="photos.length > 0"
+      />
+    </ClientOnly>
   </UContainer>
 </template>
 <script setup lang="ts">
 import { type Photo, type S3Config } from "../types";
 import { useStorage } from "@vueuse/core";
-const router =  useRouter();
+const router = useRouter();
 const toast = useToast();
 const photos: Ref<Photo[]> = useStorage("s3-photos", []);
 const s3Config = useStorage<S3Config>("s3-settings", {} as S3Config);
@@ -35,7 +37,10 @@ async function listPhotos() {
     toast.add({
       title: "Failed to list photos",
       description: "Open dev console for more info",
-      actions:[{label: "Retry", click: listPhotos}, {label: "Go to settings", click: () => router.push("/settings")}]
+      actions: [
+        { label: "Retry", click: listPhotos },
+        { label: "Go to settings", click: () => router.push("/settings") },
+      ],
     });
     console.error((error as Error).message);
   }
