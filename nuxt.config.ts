2844310fa20fbdbacb2b23bf19527459aff151bd
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-04-30 16:14:34 +0800

    feat(i18n): nuxt-i18n settings (routers broken)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@vueuse/nuxt"],
+  modules: ["@nuxt/ui", "@vueuse/nuxt", "@nuxtjs/i18n"],
   ui: {
     icons: ["mingcute"],
+  },
+  i18n: {
+    locales: [
+      {
+        code: "en",
+        flag: "us",
+        iso: "en-US",
+        file: "en.json",
+        name: "English",
+      },
+      {
+        code: "zh",
+        flag: "zh",
+        iso: "zh-CN",
+        file: "zh.json",
+        name: "中文",
+      },
+    ],
+    defaultLocale: "en",
+    strategy: "prefix",
+    langDir: "locales",
+    detectBrowserLanguage: {
+      useCookie: true,
+      // cookieKey: 'locale',
+      // fallbackLocale: 'en',
+      alwaysRedirect: false,
+      redirectOn: "root",
+    },
   },
 });
