fdcdb112241dadbc6aee89b4b6d18c831b8266c0
Chris Yang <yy4382@outlook.com>
2024-05-02 12:12:06 +0800

    fix: keep setting tab centered

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,22 +1,20 @@
 <template>
-  <UContainer :ui="{ base: 'mx-0' }">
-    <UCard class="max-w-md w-full m-auto">
-      <template #header>
-        <h2>{{ $t("settings.title") }}</h2>
+  <UCard class="max-w-md w-full m-auto">
+    <template #header>
+      <h2>{{ $t("settings.title") }}</h2>
+    </template>
+    <UTabs
+      :items="[
+        { key: 's3', label: $t('settings.s3.title') },
+        { key: 'app', label: $t('settings.app.title') },
+      ]"
+    >
+      <template #item="{ item }">
+        <ClientOnly>
+          <SettingsS3 v-if="item.key === 's3'" />
+          <SettingsApp v-if="item.key === 'app'" />
+        </ClientOnly>
       </template>
-      <UTabs
-        :items="[
-          { key: 's3', label: $t('settings.s3.title') },
-          { key: 'app', label: $t('settings.app.title') },
-        ]"
-      >
-        <template #item="{ item }">
-          <ClientOnly>
-            <SettingsS3 v-if="item.key === 's3'" />
-            <SettingsApp v-if="item.key === 'app'" />
-          </ClientOnly>
-        </template>
-      </UTabs>
-    </UCard>
-  </UContainer>
+    </UTabs>
+  </UCard>
 </template>
