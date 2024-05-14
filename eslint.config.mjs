b8d13a8d5a778f31676ea28ed6cc9fcc7628bb1c
Chris Yang <yy4382@outlook.com>
2024-05-14 22:25:50 +0800

    chore: disable vue/html-self-closing rule in eslint.config.mjs to avoid conflicts with Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,5 +4,6 @@ import withNuxt from "./.nuxt/eslint.config.mjs";
 export default withNuxt({
   rules: {
     camelcase: ["warn", { ignoreDestructuring: true }],
+    "vue/html-self-closing": ["off"], // Avoid conflicts with Prettier
   },
 });
