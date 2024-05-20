c57f5bca2930562d69fef740ab69baf2d8d7d9d9
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-05-20 14:48:22 +0800

    feat: skeleton for preview (mask on preview-image)

--- components/UploadPreviewImage.vue
+++ components/UploadPreviewImage.vue
@@ -1,12 +1,30 @@
 <template>
-  <img :src="imageUrl" />
   <!-- !TODO add key preview, compressed size preview, delete button, etc.-->
+  <div class="w-80">
+    <div
+      class="relative inline-block"
+      @mouseover="maskVisible = true"
+      @mouseleave="maskVisible = false"
+    >
+      <NuxtImg :src="imageUrl" />
+      <div
+        v-if="maskVisible"
+        class="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-black bg-opacity-50"
+      >
+        <p>hello</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
+onBeforeUnmount(() => URL.revokeObjectURL(imageUrl.value));
+
 const props = defineProps<{
   image: File;
 }>();
+
 const imageUrl = computed(() => URL.createObjectURL(props.image));
-onBeforeUnmount(() => URL.revokeObjectURL(imageUrl.value));
+
+const maskVisible = ref(false);
 </script>
