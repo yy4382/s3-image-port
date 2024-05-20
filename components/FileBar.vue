446573bd14d541c652e60d3ff7fe3db00ac24fd5
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-05-20 19:37:02 +0800

    feat: preview image on file bar

--- components/FileBar.vue
+++ components/FileBar.vue
@@ -1,26 +1,33 @@
 <template>
-  <div
-    class="flex flex-row justify-between gap-2 p-2 rounded-md hover:bg-gray-500/15 hover:text-primary-500 dark:hover:text-primary-400 transition-colors"
-    @mouseenter="hovered = true"
-    @mouseleave="hovered = false"
-  >
-    <div class="flex flex-row gap-2">
+  <UPopover mode="hover">
+    <div
+      class="flex flex-row justify-between gap-2 p-2 rounded-md hover:bg-gray-500/15 hover:text-primary-500 dark:hover:text-primary-400 transition-colors"
+      @mouseenter="hovered = true"
+      @mouseleave="hovered = false"
+    >
+      <div class="flex flex-row gap-2">
+        <span class="flex flex-col justify-center">
+          <UIcon name="i-mingcute-pic-line" />
+        </span>
+        <span class="flex flex-col justify-center">{{ props.file?.name }}</span>
+      </div>
       <span class="flex flex-col justify-center">
-        <UIcon name="i-mingcute-pic-line" />
+        <UButton
+          :padded="false"
+          color="gray"
+          variant="link"
+          icon="i-heroicons-x-mark-20-solid"
+          :class="hovered ? 'visible' : 'invisible'"
+          @click="$emit('remove', file)"
+        />
       </span>
-      <span class="flex flex-col justify-center">{{ props.file?.name }}</span>
     </div>
-    <span class="flex flex-col justify-center">
-      <UButton
-        :padded="false"
-        color="gray"
-        variant="link"
-        icon="i-heroicons-x-mark-20-solid"
-        :class="hovered ? 'visible' : 'invisible'"
-        @click="$emit('remove', file)"
-      />
-    </span>
-  </div>
+    <template #panel>
+      <div class="p-2">
+        <img :src="previewImage" class="w-72" />
+      </div>
+    </template>
+  </UPopover>
 </template>
 
 <script setup lang="ts">
@@ -29,4 +36,8 @@ const props = defineProps({
 });
 defineEmits(["remove"]);
 const hovered = ref(false);
+const previewImage = computed(() => {
+  if (!props.file) return "";
+  return URL.createObjectURL(props.file);
+});
 </script>
