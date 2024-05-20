1af1fe8af220fa2391140cf530682f7a7a67738f
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-05-20 14:48:00 +0800

    build(deps): add @nuxt/image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@vueuse/nuxt", "@nuxtjs/i18n", "@nuxt/eslint"],
+  modules: ["@nuxt/ui", "@vueuse/nuxt", "@nuxtjs/i18n", "@nuxt/eslint", "@nuxt/image"],
   ui: {
     icons: ["mingcute"],
   },
