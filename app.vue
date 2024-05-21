44a39494a224cb3c9c222cb751d14e7eeddde735
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-05-21 13:31:38 +0800

    feat: add global transition when switching lang
    See https://i18n.nuxtjs.org/docs/guide/lang-switcher#global-transition

--- app.vue
+++ app.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="min-h-screen flex flex-col dark:bg-surface-800 gap-4">
     <NavMenu />
-    <NuxtPage class="flex-grow" />
+    <NuxtPage
+      class="flex-grow"
+      :transition="{
+        name: 'my',
+        mode: 'out-in',
+        onBeforeEnter,
+      }"
+    />
     <FooterComp />
   </div>
   <UNotifications />
@@ -10,7 +17,8 @@
 <script setup lang="ts">
 useSeoMeta({
   title: "S3 Image Port",
-  description: "A dashboard to manage your images in S3 buckets. Can be used as a image hosting service.",
+  description:
+    "A dashboard to manage your images in S3 buckets. Can be used as a image hosting service.",
   ogTitle: "S3 Image Port",
   ogDescription: "A dashboard to manage your images in S3 buckets.",
   ogImage: "/cover.png",
@@ -33,4 +41,21 @@ useHead({
     },
   ],
 });
+
+const { finalizePendingLocaleChange } = useI18n();
+
+const onBeforeEnter = async () => {
+  await finalizePendingLocaleChange();
+};
 </script>
+
+<style>
+.my-enter-active,
+.my-leave-active {
+  transition: opacity 0.3s;
+}
+.my-enter,
+.my-leave-active {
+  opacity: 0;
+}
+</style>
