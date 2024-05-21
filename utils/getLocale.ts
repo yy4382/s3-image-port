066bb6ab907dcd53a92cd4bba61f046c58d728bc
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-05-21 13:43:22 +0800

    chore: remove useless util function `getLocale`

--- utils/getLocale.ts
+++
@@ -1,8 +0,0 @@
-export default function getLocale() {
-  const i18n = useI18n();
-  let locale = i18n.getLocaleCookie();
-  if (locale === undefined) {
-    locale = i18n.getBrowserLocale();
-  }
-  return locale;
-}
