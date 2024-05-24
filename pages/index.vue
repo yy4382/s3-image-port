c452cab35842b45b0ebd908db439a078a260a027
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-05-24 15:37:23 +0800

    refactor: redesign home page
    * style: remove big logo & gradient slogan
    
    * chore: modify slogan and text
    
    * fix: big weight caused overflow
    
    * chore: i18n fix (`Read the Docs` -> `Read Docs`)
    
    * chore: modify text on button
    
    * style: centralize text
    
    * style: responsive size of text and button
    
    * style: partial gradient in light theme
    
    * chore(i18n): change key name (`readTheDocs` -> `readDocs`)
    
    * style: remove logo
    
    * rev: roll back the Chinese slogan

--- pages/index.vue
+++ pages/index.vue
@@ -6,58 +6,62 @@
       class="w-full flex flex-col md:flex-row gap-12 md:gap-12 lg:gap-16 items-center px-8 md:px-0"
     >
       <div
-        class="flex flex-col space-y-4 items-start justify-center md:w-[66%]"
+        class="flex flex-col space-y-4 items-center justify-center md:w-full"
       >
-        <h1 class="text-4xl text-primary-500 dark:text-primary-400">
-          S3 Image Port
-        </h1>
-        <h2 class="text-5xl font-medium text-gray-800 dark:text-gray-200">
-          {{ $t("index.slogan") }}
-        </h2>
+        <div
+          class="text-center text-5xl md:text-6xl lg:text-8xl font-black bg-clip-text text-transparent bg-gradient-to-br from-primary-400 to-primary-600 pb-2"
+        >
+          <span class="text-gray-800 dark:text-inherit">{{
+            $t("index.slogan.part1")
+          }}</span>
+          <span>{{ $t("index.slogan.part2") }}</span>
+          <br />
+          <span class="text-gray-800 dark:text-inherit">{{
+            $t("index.slogan.part3")
+          }}</span>
+        </div>
         <div class="flex flex-col space-y-2">
-          <p class="text-xl text-gray-700 dark:text-gray-300">
+          <p
+            class="text-center text-base md:text-xl lg:text-2xl text-gray-700 dark:text-gray-300"
+          >
             {{ $t("index.description") }}
           </p>
-          <p class="text-gray-600 dark:text-gray-400">
+          <p
+            class="text-center text-sm md:text-base lg:text-xl text-gray-600 dark:text-gray-400"
+          >
             {{ $t("index.comment") }}
           </p>
         </div>
-        <div class="flex flex-wrap gap-4 pt-6">
+        <div
+          class="flex flex-wrap gap-2 sm:gap-4 md:pt-6 justify-center items-center"
+        >
           <UButton
             color="primary"
             variant="solid"
             :to="localePath('/upload')"
-            icon="i-mingcute-play-fill"
+            icon="i-mingcute-rocket-fill"
+            :size="width < 640 ? 'xs' : 'md'"
           >
-            {{ $t("index.actions.haveATry") }}
+            {{ $t("index.actions.getStarted") }}
           </UButton>
           <UButton
             color="blue"
             variant="solid"
             to="https://docs.iport.yfi.moe"
             icon="i-mingcute-book-2-fill"
+            :size="width < 640 ? 'xs' : 'md'"
           >
-            {{ $t("index.actions.readTheDocs") }}
+            {{ $t("index.actions.readDocs") }}
           </UButton>
           <UButton
+            v-if="width >= 640"
             color="gray"
             variant="solid"
             to="https://github.com/yy4382/s3-image-port"
             icon="i-mingcute-github-fill"
           >
-            {{ $t("index.actions.viewOnGithub") }}
+            {{ $t("index.actions.viewSource") }}
           </UButton>
-        </div>
-      </div>
-      <div class="relative md:w-[34%] flex justify-start">
-        <div class="w-48 h-48">
-          <img src="~/public/favicon.svg" alt="logo" class="w-48 h-48" />
-          <img
-            src="~/public/favicon.svg"
-            alt="logo"
-            draggable="false"
-            class="w-72 h-72 absolute top-1/2 left-1/2 -translate-x-[64%] -translate-y-1/2 blur-3xl opacity-0 hover:opacity-50 dark:hover:opacity-25 transition-opacity duration-700 ease-in-out"
-          />
         </div>
       </div>
     </div>
@@ -66,4 +70,5 @@
 
 <script lang="ts" setup>
 const localePath = useLocalePath();
+const { width } = useWindowSize();
 </script>
