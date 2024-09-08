02ed07bd3ab9ef9b495c8308a16311cd1f62a8ea
Chris Yang <yy4382@outlook.com>
2024-09-08 13:53:50 -0700

    refactor: settings routing

--- app/pages/settings/misc.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts" setup></script>
-
-<template>
-  <SettingsLayout>
-    <SettingsAppMisc />
-  </SettingsLayout>
-</template>
--- app/pages/settings/upload.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts" setup></script>
-
-<template>
-  <SettingsLayout>
-    <SettingsAppUpload />
-  </SettingsLayout>
-</template>
