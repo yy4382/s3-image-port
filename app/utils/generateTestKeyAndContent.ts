b0bdddc1ddd9159bd52e1a09b77a773d2a5864c7
Chris Yang <yy4382@outlook.com>
2024-11-10 16:44:32 -0800

    refactor: move `generateTestKeyAndContent` into settings

--- app/utils/generateTestKeyAndContent.ts
+++
@@ -1,6 +0,0 @@
-export default function () {
-  const timestamp = Date.now();
-  const testKey = `test-object-key-${timestamp}.txt`;
-  const testContent = `This is a test object generated at ${new Date(timestamp).toISOString()}`;
-  return { testKey, testContent };
-}
