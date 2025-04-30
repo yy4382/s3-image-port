3d47ac2fd3d29615544255fc8f8e9246c7bd473b
Chris Yang <yy4382@outlook.com>
2025-05-01 00:03:15 +0800

    refactor: use react

--- app/app.config.ts
+++
@@ -1,6 +0,0 @@
-export default defineAppConfig({
-  ui: {
-    primary: "violet",
-    gray: "cool",
-  },
-});
--- app/utils/debug.ts
+++
@@ -1,6 +0,0 @@
-export default (...args: unknown[]) => {
-  if (import.meta.env.DEV) {
-    // eslint-disable-next-line no-console
-    console.log(...args);
-  }
-};
