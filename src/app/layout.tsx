08a433c76f2d87f165f03c67a4f090dff67c184d
Chris Yang <yy4382@outlook.com>
2025-05-25 18:40:30 +0800

    chore: basic metadata and jotai provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "next-themes";
+import { Provider as JotaiProvider } from "jotai";
 import "@/app/globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "S3 Image Port",
+  icons: {
+    icon: "/favicon.svg",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -10,15 +19,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-          storageKey="s3ip:root:theme"
-        >
-          {children} <Toaster />
-        </ThemeProvider>
+        <JotaiProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="s3ip:root:theme"
+          >
+            {children} <Toaster />
+          </ThemeProvider>
+        </JotaiProvider>
       </body>
     </html>
   );
