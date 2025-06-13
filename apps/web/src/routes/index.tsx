e73101de8ecae7ca06a13ccbbc79e9e7c2113d7b
Chris Yang <yy4382@outlook.com>
2025-06-13 22:34:43 +0800

    feat: implement locale-based redirection in route component

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,15 +1,38 @@
-import { createFileRoute, Navigate } from "@tanstack/react-router";
+import { routing } from "@/i18n/routing";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { useEffect } from "react";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const lang =
+      navigator.languages
+        .map((lang) => {
+          return new Intl.Locale(lang).language;
+        })
+        .find((lang) => routing.locales.includes(lang as any)) ??
+      routing.defaultLocale;
+
+    navigate({
+      to: "/$locale",
+      params: { locale: lang },
+      replace: true,
+    });
+  }, []);
   return (
     <div>
-      <a href="/en">To English</a>
-      <a href="/zh">To Chinese</a>
-      {/* <Navigate to="/$locale" params={{ locale: "en" }} /> */}
+      <h1>Redirecting...</h1>
+      <div>
+        If you are not redirected, please click
+        <a className="underline" href="/en">
+          here
+        </a>
+      </div>
     </div>
   );
 }
