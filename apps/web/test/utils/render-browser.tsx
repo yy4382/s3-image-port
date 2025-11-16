87e319762cdff3de3fef0e75b64755b3a220a61e
Chris Yang <yy4382@outlook.com>
2025-11-17 02:03:20 +0800

    test: fix missing tanstack queryClient provider

--- apps/web/test/utils/render-browser.tsx
+++ apps/web/test/utils/render-browser.tsx
@@ -1,14 +1,18 @@
 import { IntlProvider } from "use-intl";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useState } from "react";
 import en from "@/../messages/en.json";
 import { Toaster } from "sonner";
 import { page } from "vitest/browser";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const TestProviders = ({ children }: PropsWithChildren) => {
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <IntlProvider locale="en" messages={en}>
-      {children}
-      <Toaster toastOptions={{ duration: 10000 }} />
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <Toaster toastOptions={{ duration: 10000 }} />
+      </QueryClientProvider>
     </IntlProvider>
   );
 };
