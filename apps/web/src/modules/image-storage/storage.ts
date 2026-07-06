69d30c0d762848a87ca609c47c0dce038ea7eb50
Chris Yang <yy4382@outlook.com>
2026-07-06 12:26:48 +0800

    Simplify image storage factory

--- apps/web/src/modules/image-storage/storage.ts
+++ apps/web/src/modules/image-storage/storage.ts
@@ -7,15 +7,6 @@ import type {
   StoredImageRenameResult,
   StoredImageResult,
 } from "./schema";
-import {
-  imageStorageAccessResultSchema,
-  storedImageDeleteResultSchema,
-  storedImageDownloadResultSchema,
-  storedImageListResultSchema,
-  storedImageMetadataResultSchema,
-  storedImageRenameResultSchema,
-  storedImageResultSchema,
-} from "./schema";
 
 export type StoredImageBody = Blob | string;
 
@@ -52,39 +43,5 @@ export type ImageStorage = {
 export type ImageStorageAdapter = ImageStorage;
 
 export function createImageStorage(adapter: ImageStorageAdapter): ImageStorage {
-  return {
-    async listStoredImages() {
-      return storedImageListResultSchema.parse(
-        await adapter.listStoredImages(),
-      );
-    },
-    async putStoredImage(input) {
-      return storedImageResultSchema.parse(await adapter.putStoredImage(input));
-    },
-    async deleteStoredImages(keys) {
-      return storedImageDeleteResultSchema.parse(
-        await adapter.deleteStoredImages(keys),
-      );
-    },
-    async renameStoredImage(input) {
-      return storedImageRenameResultSchema.parse(
-        await adapter.renameStoredImage(input),
-      );
-    },
-    async downloadStoredImage(key) {
-      return storedImageDownloadResultSchema.parse(
-        await adapter.downloadStoredImage(key),
-      );
-    },
-    async probeStoredImage(key) {
-      return storedImageMetadataResultSchema.parse(
-        await adapter.probeStoredImage(key),
-      );
-    },
-    async checkAccess(input) {
-      return imageStorageAccessResultSchema.parse(
-        await adapter.checkAccess(input),
-      );
-    },
-  };
+  return adapter;
 }
